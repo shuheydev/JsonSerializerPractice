8f019a4005e19a818ef63a5f5d36f0519e61b603
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2021-02-10 21:50:58 +0900

    Add project files.

---
+++ JsonSerializerPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JsonSerializerPractice
+{
+    //C:\Users\shuhei.nishizawa\Desktop\testData.json
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input json file path:");
+
+            string filePath = Console.ReadLine();
+
+            string jsonText = File.ReadAllText(filePath);
+
+            var deserialized = JsonConvert.DeserializeObject<StudentList>(jsonText);
+
+            Console.WriteLine(deserialized.Students[2].Name);
+        }
+    }
+
+    public class StudentList
+    {
+        public List<Student> Students;
+    }
+
+    public class Student
+    {
+        public string Name;
+        public int Age;
+    }
+}
+
+
+ 
